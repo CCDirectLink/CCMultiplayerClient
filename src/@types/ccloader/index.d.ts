36b7f5bc7e3a4e55b45e5741d63019567178fa7e
2767mr <2767mr@gmx.at>
2018-09-22 16:20:17 +0200

    Updated ccloader bindings

--- src/@types/ccloader/index.d.ts
+++ src/@types/ccloader/index.d.ts
@@ -2,6 +2,8 @@ declare let activeMods: Mod[];
 declare let inactiveMods: Mod[];
 declare let cc: cc;
 
+declare let getEntry: (key: string) => string
+
 /**
  * You should never be able to aquire an instance of this in a mod
  */
@@ -10,18 +12,20 @@ interface CCLoader {
 }
 
 interface Mod {
+    readonly baseDirectory: string;
+    readonly name: string;
+    readonly description: string | undefined;
+    readonly assets: string[];
+    readonly isEnabled: boolean;
+
+
     initialize(): void;
     load(cb: Function): void;
     onload(cb: Function): void;
-    getName(): string;
-    getDescription(): string | undefined;
-    getAssets(): string[];
     getAsset(path: string): string;
     setAsset(original: string, modified: string): void;
-    getBaseDirectory(): string;
     initializeTable(ccloader: CCLoader, cb: Function): void;
     executeTable(ccloader: CCLoader): void;
-    isEnabled(): boolean;
 }
 
 interface cc {
