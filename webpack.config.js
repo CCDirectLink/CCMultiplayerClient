65a17ae78b1b7f7e42e12d0e035f35adbd108ed6
2767mr <2767mr@gmx.at>
2018-09-25 21:37:51 +0200

    Fixed wrong build script

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ const config = {
     plugins: [
         new CopyWebpackPlugin([{
             context: 'assets/',
-            from: '*/**',
+            from: '**',
             to: '[path][name].[ext]'
         }])
     ],
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -11,7 +11,7 @@ const config = {
     plugins: [
         new CopyWebpackPlugin([{
             context: 'assets/',
-            from: '*/**',
+            from: '**',
             to: '[path][name].[ext]'
         }])
     ],
